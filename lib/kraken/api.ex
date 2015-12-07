18f15f1907e46c59748ab18869e56f1ab7414f53
Andrey Parubets <parubets@gmail.com>
2015-12-07 20:44:29 +0100

    open orders added

--- lib/kraken/api.ex
+++ lib/kraken/api.ex
@@ -4,6 +4,16 @@ defmodule Kraken.Api do
     post_to_api "/0/private/Balance"
   end
 
+  def trade_balance do
+    post_to_api "/0/private/TradeBalance"
+  end
+
+  def open_orders(opts \\ []) do
+    trades = Keyword.get(opts, :trades)
+    userref = Keyword.get(opts, :userref)
+    post_to_api "/0/private/OpenOrders", reduce_params(%{trades: trades, userref: userref})
+  end
+
   def trade_volume(opts \\ []) do
     pair = Keyword.get(opts, :pair)
     post_to_api "/0/private/TradeVolume", reduce_params(%{pair: pair})
