75e1d033c8bbf073a0c64acd67501120bb9b7fbc
Andrey Parubets <parubets@gmail.com>
2018-02-05 14:41:26 +0100

    deps up

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule KrakenElixir.Mixfile do
 
   def project do
     [app: :kraken_elixir,
-     version: "0.3.0",
+     version: "0.3.2",
      elixir: "~> 1.1",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
@@ -20,8 +20,8 @@ defmodule KrakenElixir.Mixfile do
 
   defp deps do
     [
-      {:httpoison, "~> 0.11.2"},
-      {:poison, "~> 3.1"}
+      {:httpoison, "~> 1.0"},
+      {:poison, "~> 3.1"},
     ]
   end
 end
